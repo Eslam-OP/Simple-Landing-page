df19415e797ed4aafbc565d797fd8ff74924922f
Eslam-OP <123605401+Eslam-OP@users.noreply.github.com>
2025-05-07 21:04:00 +0300

    project.js

--- project.js
+++ project.js
@@ -159,3 +159,35 @@ span.onclick = function () {
     top: 0,
   });
 };
+
+// Toggle Menu
+let toggleBtn = document.querySelector(".toggle-menu");
+let tLinks = document.querySelector(".links");
+
+toggleBtn.onclick = function (e) {
+  // Stop Propagation on Toggle Btn
+  e.stopPropagation();
+
+  // Toggle Class "menu-active" On Button
+  this.classList.toggle("menu-active");
+
+  // Toggle Class "open" On Links
+  tLinks.classList.toggle("open");
+};
+
+// Click Anywhere outside Menu And Toggle Btn
+document.addEventListener("click", (e) => {
+  if (e.target !== toggleBtn && e.target !== tLinks) {
+    if (tLinks.classList.contains("open")) {
+      // Toggle Class "menu-active" On Button
+      toggleBtn.classList.toggle("menu-active");
+      // Toggle Class "open" On Links
+      tLinks.classList.toggle("open");
+    }
+  }
+});
+
+// Stop Propagation on Menu
+tLinks.onclick = (e) => {
+  e.stopPropagation();
+};
